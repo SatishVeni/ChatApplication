5cc4584aae519cd45913227b6f98776fdbd72429
satish <satishkumar_venigalla@trimble.com>
2018-11-06 21:04:01 +0530

    test

--- SampleChat/ChatHub.cs
+++ SampleChat/ChatHub.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
-using SampleChat.MatrixSvc;
 using System.Threading.Tasks;
 using Matrix.Xmpp.Client;
 using Matrix.Xmpp;
